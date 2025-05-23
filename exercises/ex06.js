04fa12f0ccfed1ce7c5fda278ae1e23df6ee0fe0
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-05-22 19:55:32 -0700

    Updated ex06.js

--- exercises/ex06.js
+++ exercises/ex06.js
@@ -1,9 +1,13 @@
 /*
-From the smallest of creatures to the largest of animals, inevitably every living, breathing thing must ingest other organisms to survive. This means that all animals will fall within one of the three consumer-based categories based on the types of food that they eat.
+From the smallest of creatures to the largest of animals, inevitably every living, breathing thing must ingest other organisms to survive. 
+This means that all animals will fall within one of the three consumer-based categories based on the types of food that they eat.
 
-Animals that eat only plants are called herbivores
+Animals that eat only plants are called herbivores 
+-> (eatsPlants && !eatsAnimals)
 Animals that eat only other animals are called carnivores
+-> (!eatsPlants && eatsAnimals)
 Animals that eat both plants and animals are called omnivores
+-> (eatsPlants && eatsAnimals)
 Directions:
 Write a series of ternary statements that sets the variable category equal to:
 
@@ -15,3 +19,16 @@ Use the eatsPlants and eatsAnimals variables to test your code.
 
 If eatsPlants equals true and eatsAnimals equals false, then herbivore should be printed to the console. 
 */
+
+let eatsPlants = true
+let eatsAnimals = false
+
+if (eatsPlants && !eatsAnimals) {
+    console.log("herbivores")
+} else if (!eatsPlants && eatsAnimals) {
+    console.log("carnivore")
+} else if (eatsPlants && eatsAnimals) {
+    console.log("omnivore")
+} else {
+    console.log("undefined")
+}
