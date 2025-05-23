9043826a0890ff88c8666ff89e69879486887f60
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-05-22 21:14:01 -0700

    Updated ex05.js

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -8,7 +8,8 @@ XL	    24"	    31"	    8.88"
 2XL	    26"	    33"	    9.63"
 3XL	    28"	    34"	    10.13"
 
-Use the sizing chart above, create a series of logical expressions that prints the size of a t-shirt based on the measurements of shirtWidth, shirtLength, and shirtSleeve. Valid sizes include S, M, L, XL, 2XL, and 3XL.
+Use the sizing chart above, create a series of logical expressions that prints the size of a t-shirt based on the measurements of shirtWidth, shirtLength, and shirtSleeve. 
+Valid sizes include S, M, L, XL, 2XL, and 3XL.
 
 For example, if...
 
@@ -18,7 +19,8 @@ const shirtSleeve = 8.71; // size L (large)
 
 Then print L to the console. 
 
-If shirtWidth, shirtLength, and shirtSleeve don't fit within the range of acceptable values for a specific size, then print NA to the console. For example, if...
+If shirtWidth, shirtLength, and shirtSleeve don't fit within the range of acceptable values for a specific size, 
+then print NA to the console. For example, if...
 
 var shirtWidth = 18; // size S (small)
 var shirtLength = 29; // size M (medium)
@@ -26,3 +28,23 @@ var shirtSleeve = 8.47; // size M (medium)
 Then print N/A to the console because the measurements don't all match up with one particular size.
 
 */
+
+var shirtWidth = 18; // size S (small)
+var shirtLength = 29; // size M (medium)
+var shirtSleeve = 8.47; // size M (medium)
+
+if ((shirtWidth < 20 & shirtWidth >= 18) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve <= 8.13 && shirtSleeve < 8.38)) {
+    console.log("S")
+} else if ((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)) {
+    console.log("M")
+} else if ((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)) {
+    console.log("L")
+} else if ((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 32) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)) {
+    console.log("XL")
+} else if ((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 32 && shirtLength < 33) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)) {
+    console.log("2XL")
+} else if ((shirtWidth >= 28) && (shirtLength >= 33) && (shirtSleeve >= 10.13)) {
+    console.log("3XL")
+} else {
+    console.log("N/A")
+}
