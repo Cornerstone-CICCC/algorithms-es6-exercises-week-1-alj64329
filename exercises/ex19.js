772a8fa5f8b31a4ca74089f8b51499f396877504
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-05-23 20:41:05 -0700

    Updated ex19.js

--- exercises/ex19.js
+++ exercises/ex19.js
@@ -1,6 +1,20 @@
 /*
-Call the emotions() function so that it prints the output you see below, but instead of passing the laugh() function as an argument, pass an inline function expression instead.
+Call the emotions() function so that it prints the output you see below, 
+but instead of passing the laugh() function as an argument, pass an inline function expression instead.
 
 emotions("happy", laugh(2)); // you can use your laugh function from the previous quizzes
 Prints: "I am happy, haha!"
 */
+
+function emotions(par, func) {
+    let num = 2
+    console.log(`I am ${par}, ${func(num)}`)
+}
+
+emotions("happy", function laugh(num) {
+    let msg = ""
+    for (let i = 1; i <= num; i++) {
+        msg += "ha"
+    }
+    return msg + "!"
+}); 
