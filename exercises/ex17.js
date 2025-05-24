48e6cbf776d1eaa0358b480a88a9d8e612c2cd25
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-05-23 20:34:24 -0700

    Updated ex17.js

--- exercises/ex17.js
+++ exercises/ex17.js
@@ -4,3 +4,13 @@ Write an anonymous function expression that stores a function in a variable call
 laugh(3);
 Returns: hahaha!
 */
+
+const laugh = function (num) {
+    let msg = ""
+    for (let i = 1; i <= num; i++) {
+        msg += "ha"
+    }
+    return msg + "!"
+}
+
+console.log(laugh(3))
