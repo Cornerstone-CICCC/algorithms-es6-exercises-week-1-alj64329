c327423b944ac0f9dcbafeb1d68014dee5b186ab
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-05-27 14:46:05 -0700

    ex21.js

--- exercises/ex21.js
+++ exercises/ex21.js
@@ -6,3 +6,15 @@ TIP: The 1st element of any array has an index of 0.
 
 Afterwards, print out the prices array to the console.
 */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+console.log(prices)
+
+prices[0] = 1;
+
+prices[2] = 90
+
+prices[6] = 8.00
+
+console.log(prices)
